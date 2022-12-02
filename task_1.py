8444d8e8e39df4dad6e4e14a9fcb60de7d536c05
Valentin447 <valentin@myttk.ru>
2022-12-03 02:33:42 +0700

    Add solution task_1

--- task_1.py
+++ task_1.py
@@ -1,1 +1,16 @@
+'''
+1) Реализовать скрипт, в котором должна быть предусмотрена функция расчета
+заработной платы сотрудника. В расчете необходимо использовать формулу:
+(выработка в часах*ставка в час) + премия. Для выполнения расчета для
+конкретных значений необходимо запускать скрипт с параметрами.
+'''
+from sys import argv
 
+script_name, working_time, working_rate, bonus = argv
+
+
+def salary(wt, wr, b):
+    return int(wt) * int(wr) + int(b)
+
+
+print(salary(wt=working_time, wr=working_rate, b=bonus))
